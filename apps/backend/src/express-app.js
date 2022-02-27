b43d34732e48a981d17bdf707a97b86a6b00f4a3
Cristian Incarnato <cristian.cdi@gmail.com>
2022-02-26 23:00:49 -0300

    Error handler Middleware

--- apps/backend/src/express-app.js
+++ apps/backend/src/express-app.js
@@ -1,9 +1,12 @@
+
+
 require('dotenv').config();
 import express from 'express';
 
 import {jwtMiddleware, corsMiddleware, rbacMiddleware, sessionMiddleware} from '@dracul/user-backend'
 import {ResponseTimeMiddleware,RequestMiddleware} from '@dracul/logger-backend'
 import unauthorizedErrorMiddleware from "./middlewares/UnauthorizedErrorMiddleware";
+import ErrorHandlerMiddleware from "./middlewares/ErrorHandlerMiddleware";
 
 import mediaRoute from "./routes/MediaRoute";
 import statusRoute from "./routes/StatusRoute";
@@ -23,5 +26,8 @@ expressApp.use(sessionMiddleware)
 //Routes
 expressApp.use(mediaRoute)
 expressApp.use(statusRoute)
+
+//Error handler Middleware
+expressApp.use(ErrorHandlerMiddleware);
 
 export default expressApp
