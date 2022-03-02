4070eff0cc4d42a8dc490f76e433f3e96616085c
Cristian Incarnato <cristian.cdi@gmail.com>
2022-03-02 17:37:25 -0300

    Scaffold funcionando con Refresh Token

--- apps/frontend/src/apollo/index.js
+++ apps/frontend/src/apollo/index.js
@@ -1,26 +1,26 @@
 import {ApolloClient} from 'apollo-client'
-import {createUploadLink} from 'apollo-upload-client'
 import {InMemoryCache} from 'apollo-cache-inmemory'
-import {ApolloLink} from 'apollo-link'
+import { setContext } from "apollo-link-context";
 import store from '../store'
-
+import {createUploadLink} from 'apollo-upload-client'
 import {onError} from "apollo-link-error";
+import {ApolloLink} from 'apollo-link'
 
 const errorLink = onError(({graphQLErrors, networkError}) => {
     if (graphQLErrors)
         graphQLErrors.forEach(
-            (graphQlError,index) => {
+            (graphQlError, index) => {
 
-                const { message, locations, path, extensions} = graphQlError
+                const {message, locations, path, extensions} = graphQlError
 
                 //Show error on console
                 console.error(`[GraphQL error]: Message: ${message}, Code: ${extensions.code}, Path: ${path}, Location ${locations}`)
 
                 //Add error for errorSnackbar
-                setTimeout(() => store.commit('addGraphqlError',graphQlError),100* (index+1) )
+                setTimeout(() => store.commit('addGraphqlError', graphQlError), 100 * (index + 1))
 
                 //Handle UNAUTHENTICATED errors
-                if(extensions.code === 'UNAUTHENTICATED'){
+                if (extensions.code === 'UNAUTHENTICATED') {
                     store.dispatch('checkAuth')
                 }
 
@@ -30,7 +30,7 @@ const errorLink = onError(({graphQLErrors, networkError}) => {
     if (networkError) {
         console.error(`[Network error]: ${networkError}, message: ${networkError.message}`);
         //Add error for errorSnackbar
-        setTimeout(() => store.commit('addGraphqlError',networkError),10 )
+        setTimeout(() => store.commit('addGraphqlError', networkError), 10)
 
     }
 });
@@ -39,22 +39,28 @@ const uploadLink = createUploadLink({
     uri: (process.env.VUE_APP_APIHOST ? process.env.VUE_APP_APIHOST : '') + '/graphql/',
 })
 
-//Middleware for Authorization
-const authLink = new ApolloLink((operation, forward) => {
-    if (store.getters.getToken) {
-        operation.setContext({
-            headers: {
-                Authorization: 'bearer ' + store.getters.getToken
-            }
-        });
-    }
+const apolloLink = new ApolloLink((operation, forward) => {
     return forward(operation);
+})
+
+
+//Middleware for Authorization
+const authLink = setContext(async (req, { headers }) => {
+    let context = {headers: {...headers}}
+    if(req.operationName != "refreshToken"){
+        let sessionIsValid = await store.dispatch('validateSession')
+        if (sessionIsValid) {
+            context.headers.Authorization = 'bearer ' + store.getters.getToken
+        }
+    }
+    return context
 })
 
 
 const link = ApolloLink.from([
     errorLink,
     authLink,
+    apolloLink,
     uploadLink
 ]);
 
