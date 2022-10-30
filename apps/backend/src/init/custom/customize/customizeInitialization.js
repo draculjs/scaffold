6fa5d99a956d7dc614ec213b4bb30177ac689bfd
Cristian Incarnato <cristian.cdi@gmail.com>
2022-10-30 11:57:07 -0300

    Dark mode complete!

--- apps/backend/src/init/custom/customize/customizeInitialization.js
+++ apps/backend/src/init/custom/customize/customizeInitialization.js
@@ -1,20 +1,22 @@
 export const customizeInitialization = {
     logo: {mode: 'OnlyTitle', title: 'APP'},
     language: "es",
-    darkTheme: {
-        primary: '#3F51B5',
-        onPrimary: '#FFFFFF',
-        secondary: '#1565C0',
-        onSecondary: '#FFFFFF',
-        background: "#F5F5F5",
-        appBar: "#D6D6D6",
-    },
     lightTheme: {
         primary: '#71DDC7',
         onPrimary: '#000000',
         secondary: '#E57FFB',
         onSecondary: '#010101',
         background: "#121212",
+        appBar: "#D6D6D6",
+    },
+    darkTheme: {
+        primary: '#3F51B5',
+        onPrimary: '#FFFFFF',
+        secondary: '#1565C0',
+        onSecondary: '#FFFFFF',
+        background: "#F5F5F5",
         appBar: "#000000",
-    }
+
+    },
+
 }
