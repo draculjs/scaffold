490fb3194d0f76d161068f2b8c5b017db0c389f1
cincarnato <cristian.cdi@gmail.com>
2023-11-16 08:23:50 -0300

    fix: Load settings store

--- apps/frontend/src/store/index.js
+++ apps/frontend/src/store/index.js
@@ -2,17 +2,20 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import {UserModuleStore} from '@dracul/user-frontend'
 import {CustomizationStore} from '@dracul/customize-frontend'
+import {SettingsModuleStore} from "@dracul/settings-frontend";
 import BaseModuleStore from '../modules/base/storage/BaseModuleStore'
 
 Vue.use(Vuex)
 
 import createPersistedState from "vuex-persistedstate";
 
+
 export default new Vuex.Store({
     modules:{
         user: UserModuleStore,
         base: BaseModuleStore,
-        customization: CustomizationStore
+        customization: CustomizationStore,
+        settings: SettingsModuleStore
     },
     plugins: [
         createPersistedState({
